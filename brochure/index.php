c720fbe85a38cf25491719bb559347c4355b5de7
Roger Miret <roger.miret@gmail.com>
2018-01-16 11:06:47 +0100

    real redirect

--- brochure/index.php
+++ brochure/index.php
@@ -4,7 +4,6 @@ $loc = "https://drive.google.com/uc?authuser=0&id=1a5jDaZkpavE0JL3dbl_VEFxd2ET97
 
 if (isset($_GET['download'])) $loc = $loc . "&export=download";
 
-die($loc);
 header("Location: " . $loc);
 
 ?>
